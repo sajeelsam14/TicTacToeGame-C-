2b38b783ec08c043776ff2d3d98cd56d3062b5b3
Sajeel Ahmed <123180871+sajeelsam14@users.noreply.github.com>
2024-03-27 15:06:56 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
-char board[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
-char token = 'x';
-int position;
-void drawBoard() {
-  cout << board[0] << "|" << board[1] << "|" << board[2] << endl;
+
+void drawBoard(char *board) {
+  cout << *board << "|" << *(board + 1) << "|" << *(board + 2) << endl;
   cout << "______" << endl;
-  cout << board[3] << "| " << board[4] << "|" << board[5] << endl;
+  cout << *(board + 3) << "| " << *(board + 4) << "|" << *(board + 5) << endl;
   cout << "______" << endl;
-  cout << board[6] << "| " << board[7] << "|" << board[8] << endl;
+  cout << *(board + 6) << "| " << *(board + 7) << "|" << *(board + 8) << endl;
 }
-void placment() {
-  if (token == 'x') {
+
+void placment(char *board, char *token) {
+  int position;
+  if (*token == 'x') {
     cout << "player one turn" << endl << "Enter the position: ";
     cin >> position;
     if (position >= 0 && position <= 8) {
       if (board[position] != 'x' && board[position] != 'o') {
         board[position] = 'x';
-        token = 'o';
+        *token = 'o';
       } else {
         cout << "Place already filled" << endl;
-        placment();
+        placment(board, token);
       }
     } else {
       cout << "Invalid position" << endl;
-      placment();
+      placment(board, token);
     }
   } else {
     cout << "player second turn" << endl << "Enter the position: ";
@@ -32,49 +32,52 @@ void placment() {
     if (position >= 0 && position <= 8) {
       if (board[position] != 'x' && board[position] != 'o') {
         board[position] = 'o';
-        token = 'x';
+        *token = 'x';
       } else {
         cout << "Place already filled" << endl;
-        placment();
+        placment(board, token);
       }
     } else {
       cout << "Invalid position" << endl;
-      placment();
+      placment(board, token);
     }
   }
 }
-bool isDraw() {
-  if (board[0] == '0' || board[1] == '1' || board[2] == '2' ||
-      board[3] == '3' || board[4] == '4' || board[5] == '5' ||
-      board[6] == '6' || board[7] == '7' || board[8] == '8') {
+
+bool isDraw(char *board) {
+  if (*board == '0' || *(board + 1) == '1' || *(board + 2) == '2' ||
+      *(board + 3) == '3' || *(board + 4) == '4' || *(board + 5) == '5' ||
+      *(board + 6) == '6' || *(board + 7) == '7' || *(board + 8) == '8') {
     return true;
   } else {
     return false;
   }
 }
-bool conclusion() {
-  if (((board[0] == board[1]) and (board[1] == board[2])) or
-      ((board[3] == board[4]) and (board[4] == board[5])) or
-      ((board[6] == board[7]) and (board[7] == board[8])) or
-      ((board[0] == board[3]) and (board[3] == board[6])) or
-      ((board[1] == board[4]) and (board[4] == board[7])) or
-      ((board[2] == board[5]) and (board[5] == board[8]))) {
-    if (token == 'x') {
+
+bool conclusion(char *board, char *token) {
+  if (((*board == *(board + 1)) && (*(board + 1) == *(board + 2))) ||
+      ((*(board + 3) == *(board + 4)) && (*(board + 4) == *(board + 5))) ||
+      ((*(board + 6) == *(board + 7)) && (*(board + 7) == *(board + 8))) ||
+      ((*board == *(board + 3)) && (*(board + 3) == *(board + 6))) ||
+      ((*(board + 1) == *(board + 4)) && (*(board + 4) == *(board + 7))) ||
+      ((*(board + 2) == *(board + 5)) && (*(board + 5) == *(board + 8)))) {
+    if (*token == 'x') {
       cout << "player2 win";
     } else {
       cout << "player1 win";
     }
     return false;
-  } else if (((board[0] == board[4]) and (board[4] == board[8])) or
-             ((board[2] == board[4]) and (board[4] == board[6]))) {
-    if (token == 'x') {
+  } else if (((*board == *(board + 4)) && (*(board + 4) == *(board + 8))) ||
+             ((*(board + 2) == *(board + 4)) &&
+              (*(board + 4) == *(board + 6)))) {
+    if (*token == 'x') {
       cout << "player2 wins";
     } else {
       cout << "player1 wins";
     }
     return false;
   } else {
-    if (isDraw()) {
+    if (isDraw(board)) {
       return true;
     } else {
       cout << "match Draw";
@@ -83,11 +86,13 @@ bool conclusion() {
   }
 }
 int main() {
-  drawBoard();
-  while (conclusion()) {
-    placment();
-    drawBoard();
+  char board[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+  char token = 'x';
+  drawBoard(board);
+  while (conclusion(board, &token)) {
+    placment(board, &token);
+    drawBoard(board);
   }
-  cout << "Program Terminated";
+  cout << "\nProgram Terminated";
   return 0;
 }
